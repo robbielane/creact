b3fc55ceb3f6bb13b75f8b84df8c799d6c0cd016
Robbie Lane <lane.robbie@gmail.com>
2015-12-22 09:50:19 -0700

    final

--- app/assets/javascripts/components/_skill.js.jsx
+++ app/assets/javascripts/components/_skill.js.jsx
@@ -17,7 +17,7 @@ var Skill = React.createClass({
 
 
   handleLevelChange(action) {
-    let levels  = ['bad', 'halfbad', 'fantastic']
+    let levels  = ['bad', 'halfbad', 'fantastic'];
     let name    = this.props.skill.name;
     let details = this.props.skill.details;
     let level   = this.props.skill.level;
